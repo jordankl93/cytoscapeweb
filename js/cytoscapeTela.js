98fa79fe11a07303ec7e72fe697dd12fe95b5a62
jordankl93 <jordankl93@gmail.com>
2016-06-03 00:38:55 -0300

    incremento nas configurações

--- js/cytoscapeTela.js
+++ js/cytoscapeTela.js
@@ -41,6 +41,7 @@ $(function () {
      </div>\
      ');*/
     leitorTXT.onload = leTXT;
+	var d1, d2;
     var div_id = "cytoscapeweb";
     var vis;
 
@@ -92,7 +93,7 @@ $(function () {
         leitorTXT.readAsText(file);
     }
     
-    //Função le o arquivo e exibe na tela
+    //Função le o arquivo e exibe na tela - antiga função draw
     function leTXT(evt) {
         var fileArr = evt.target.result;
         fileArr = fileArr.replace(/\t/g, " x ");
@@ -117,63 +118,174 @@ $(function () {
             _srcId = null;
         }
     }
+	
+	function showElapsedTime() {
+		d2 = new Date();
+		var elapsed = (d2.getTime() - d1.getTime()) / 1000;
+		$("#elapsedTime").html(elapsed + " seconds");
+	}
+	
+	function createData(nodesNumber, edgesNumber) {
+		var data = '<graphml>' +
+				   '<key attr.type="string" attr.name="label" for="all" id="label"/>' +
+				   '<key attr.type="double" attr.name="weight" for="all" id="weight"/>' +
+				   '<graph edgedefault="undirected">';
 
-    $("input").attr("disabled", true);
+		var i;
+		for (i = 1; i <= nodesNumber; i++) {
+			data += '<node id="'+i+'">' +
+					'<data key="weight">'+Math.random()+'</data>' +
+					'<data key="label">Node '+i+'</data>' +
+					'</node>';
+		}
+		if (nodesNumber > 1) {
+			for (i = 1; i <= edgesNumber; i++) {
+				var src = 0, tgt = 0;
+				while (src <= 0) {
+					src = Math.round(Math.random() * nodesNumber);
+				}
+				while (tgt <= 0 || tgt === src) {
+					tgt = Math.round(Math.random() * nodesNumber);
+				}
+				
+				data += '<edge id="'+i+'" source="'+src+'" target="'+tgt+'">' +
+						'<data key="weight">'+Math.random()+'</data>' +
+						'<data key="label">Edge '+i+'</data>' +
+						'</edge>';
+			}
+		}
+				   
+		data += '</graph></graphml>';
+
+		return data;
+	}
+	
+	function draw(data) {
+		$("input, select").attr("disabled", true);
+
+		if (data) {
+			var layout = { name: "ForceDirected" };
+
+			if (data.indexOf("</graphml>") === -1 && data.indexOf("</graph>") > -1) { // XGMML...
+				layout = { name: "Preset", options: { fitToScreen: true } };
+			} else { // GraphML or SIF
+				if ($("#layouts").val() !== "Preset") {
+					layout = { name: $("#layouts").val(), options: { weightAttr: "weight", restLength: 50, autoStabilize: false } };
+				}
+			}
+
+			options.layout = layout;
+			options.network = data;
+			options.nodeLabelsVisible = $("#showNodeLabels").is(":checked");
+			options.edgeLabelsVisible = $("#showEdgeLabels").is(":checked");
+
+			d1 = new Date();
+			vis.draw(options);
+		}
+	}
 
     // init and draw
     vis = new org.cytoscapeweb.Visualization(div_id, {swfPath: "swf/CytoscapeWeb", flashInstallerPath: "swf/playerProductInstall"});
 
     vis.ready(function () {
-        var layout = vis.layout();
-        $("input, select").attr("disabled", false);
-        $("#showNodeLabels").attr("checked", vis.nodeLabelsVisible());
+        showElapsedTime();
+
+		var layout = vis.layout();
+		$("#layouts").val(layout.name);
+		$("input, select").attr("disabled", false);
+		$("#updateVisualStyle").attr("disabled", false);
+		$("#mergeEdges").attr("checked", vis.edgesMerged());
+		$("#showNodeLabels").attr("checked", vis.nodeLabelsVisible());
+		$("#showEdgeLabels").attr("checked", vis.edgeLabelsVisible());
 
         vis.addContextMenuItem("Deletar nó", "nodes", function (evt) {
             vis.removeNode(evt.target.data.id, true);
         })
-                .addContextMenuItem("Deletar aresta", "edges", function (evt) {
-                    vis.removeEdge(evt.target.data.id, true);
-                })
-                .addContextMenuItem("Adicionar novo nó", function (evt) {
-                    var x = evt.mouseX;
-                    var y = evt.mouseY;
-                    var parentId;
-                    if (evt.target != null && evt.target.group == "nodes") {
-                        parentId = evt.target.data.id;
-                        x = evt.target.x;
-                        y = evt.target.y;
-                        x += Math.random() * (evt.target.width / 3) * (Math.round(Math.random() * 100) % 2 == 0 ? 1 : -1);
-                        y += Math.random() * (evt.target.height / 3) * (Math.round(Math.random() * 100) % 2 == 0 ? 1 : -1);
-                    }
-                    var n = vis.addNode(x, y, {parent: parentId}, true);
-                    n.data.label = n.data.id;
-                    vis.updateData([n]);
-                })
-                .addContextMenuItem("Adicionar nova aresta (Clique sobre o nó alvo)", "nodes", function (evt) {
-                    _srcId = evt.target.data.id;
-                    vis.removeListener("click", "nodes", clickNodeToAddEdge);
-                    vis.addListener("click", "nodes", clickNodeToAddEdge);
-                })
-                .addContextMenuItem("Deletar selecionados", function (evt) { //Deleta Nós e arestas selecionados
-                    var items = vis.selected();
-                    if (items.length > 0) {
-                        vis.removeElements(items, true);
-                    }
-                });
+		.addContextMenuItem("Deletar aresta", "edges", function (evt) {
+			vis.removeEdge(evt.target.data.id, true);
+		})
+		.addContextMenuItem("Adicionar novo nó", function (evt) {
+			var x = evt.mouseX;
+			var y = evt.mouseY;
+			var parentId;
+			if (evt.target != null && evt.target.group == "nodes") {
+				parentId = evt.target.data.id;
+				x = evt.target.x;
+				y = evt.target.y;
+				x += Math.random() * (evt.target.width / 3) * (Math.round(Math.random() * 100) % 2 == 0 ? 1 : -1);
+				y += Math.random() * (evt.target.height / 3) * (Math.round(Math.random() * 100) % 2 == 0 ? 1 : -1);
+			}
+			var n = vis.addNode(x, y, {parent: parentId}, true);
+			n.data.label = n.data.id;
+			vis.updateData([n]);
+		})
+		.addContextMenuItem("Adicionar nova aresta (Clique sobre o nó alvo)", "nodes", function (evt) {
+			_srcId = evt.target.data.id;
+			vis.removeListener("click", "nodes", clickNodeToAddEdge);
+			vis.addListener("click", "nodes", clickNodeToAddEdge);
+		})
+		.addContextMenuItem("Deletar selecionados", function (evt) { //Deleta Nós e arestas selecionados
+			var items = vis.selected();
+			if (items.length > 0) {
+				vis.removeElements(items, true);
+			}
+		});
     });
 
-    vis.addListener("error", function (err) {
-        alert(err.value.msg);
-    });
-
-    // Register control liteners:
+    vis.addListener("layout", function(evt) {
+		showElapsedTime();
+	})
+	.addListener("dblclick", function(evt) {
+		vis.addNode(evt.mouseX, evt.mouseY, { weight: Math.random() }, true);
+	})
+	.addListener("error", function(err) {
+		alert(err.value.msg);
+	});
+	
+	// Register control liteners:
+	$("#layouts").change(function(evt) {
+		d1 = new Date();
+		vis.layout($("#layouts").val());
+	});
     $("#showNodeLabels").change(function (evt) {
         vis.nodeLabelsVisible($("#showNodeLabels").is(":checked"));
     });
+	$("#showEdgeLabels").change(function(evt) {
+		vis.edgeLabelsVisible($("#showEdgeLabels").is(":checked"));
+	});
     $("#reapplyLayout").click(function (evt) {
         var layout = vis.layout();
         vis.layout(layout);
-    });
+    });	
+	$("#createGraph").click(function(evt) {
+		var data = createData($("#nodesNumber").val(), $("#edgesNumber").val());
+		draw(data);
+	});
+	$("#minNodeWeight, #minEdgeWeight").keydown(function(evt) {
+		if (evt.which === 13) {
+		   var val = parseFloat($(this).val());
+		   var reapplyMappers = $("#reapplyMappers").is(":checked");
+		   val = isNaN(val) ? 0 : val;
+		   var gr = $(this).attr("id") === "minNodeWeight" ? "nodes" : "edges";
+		   vis.filter(gr, function(item) {
+				return item.data.weight >= val;
+		   }, reapplyMappers);
+		   // Enable the update button when the filter did not update the view:
+		   $("#updateVisualStyle").attr("disabled", reapplyMappers);
+		}
+	});
+	$("#updateVisualStyle").click(function(evt) {
+		vis.visualStyle(options.visualStyle);
+	});
+	$("#toXGMML").click(function(evt) {
+		alert(vis.xgmml());
+	});
+	$("#toGraphML").click(function(evt) {
+		alert(vis.graphml());
+	});
+	$("#toSIF").click(function(evt) {
+		alert(vis.sif());
+	});
     
     /*Quando um arquivo é chamado*/
     $("#fileCytoscape").change(function (evt) {
